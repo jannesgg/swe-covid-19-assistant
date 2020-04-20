e475a519a1fb20f7227ceedc134e6600aa24aab3
Benedikt Ziegler <bziegler@users.noreply.github.com>
2020-04-20 21:52:09 +0200

    Improve UI for all languages

--- app/app.py
+++ app/app.py
@@ -31,13 +31,65 @@ df = pd.read_csv("../data/corona_data.csv")
 embedding_mat = model(list(df.question.values)).numpy()
 # embedding_mat = model.encode(list(df.question.values))
 
-BLOB_RE = re.compile(r"(?P<text>.*?)"
-                     r"( [Uu]pdated: (?P<updated>\d{4}-\d{2}-\d{2} \d{2}:\d{"
-                     r"2}))"
-                     r"( \[[Oo]pen in [Nn]ew [Tt]ab\] \(.*(?P<url>\? [Ee]xp "
-                     r"= [\d,]+ # _\d+)\))?")
+UPDATED_RE = re.compile(r"(\s\w+: (?P<updated>\d\d\d\d-\d\d-\d\d\s\d\d:\d\d))")
+LINK_RE = re.compile(r"(\s\[(?P<label>.*?)\]\((?P<url>.+?)\))")
 
 LANGUAGES = ["ar", "en", "es", "fr", "de", "nl"]
+
+
+def prepare_source_link(text, links, lang):
+    source_link = links.pop(-1)
+    text = text.replace(source_link["id"], "")
+
+    source_link.pop("id")
+    url = source_link["url"]
+    source_link["url"] = f"?exp={url.rsplit('?exp=', 1)[1]}"
+    source_link["label"] = translate(source_link["label"], lang)
+
+    return text, source_link
+
+
+def get_and_replace_links(text):
+    link_matches = LINK_RE.findall(text)
+    links = []
+
+    for i, link in enumerate(link_matches):
+        link_id = f"{i}" * 5
+        text = text.replace(f"{link[0]}", link_id)
+        links.append({"id": link_id, "label": link[1], "url": link[2]})
+
+    return text, links
+
+
+def insert_links(text, links, lang):
+    for link in links:
+        t_link_label = translate(link["label"], lang).capitalize()
+        link_rep = f'<a href="https://www.folkhalsomyndigheten.se' \
+                   f'{link["url"].replace(" ", "")}">' \
+                   f'{t_link_label}</a>'
+        text = text.replace(link["id"], link_rep)
+
+    return text
+
+
+def get_and_replace_updated(text):
+    updated_match = UPDATED_RE.search(text)
+    updated = updated_match["updated"]
+    text = text.replace(updated_match.group(0), "")
+    return text, updated
+
+
+def translate(text, lang):
+    return translator.translate(text, dest=lang).text
+
+
+def translate_answer(answer, lang):
+    answer, updated = get_and_replace_updated(answer)
+    answer, links = get_and_replace_links(answer)
+    answer, source_link = prepare_source_link(answer, links, lang)
+    t_answer = translate(answer, lang)
+    t_answer = insert_links(t_answer, links, lang)
+    return t_answer, updated, source_link
 
 
 @app.route("/")
@@ -55,38 +107,32 @@ def query_check():
     query_text = request.form["text"]
     query = model([query_text])
     query_mat = np.full((len(df), np.array(embedding_mat).shape[1]), query)
-    cosim_matrix = 1 - distance.cdist(query_mat, embedding_mat,
-                                      "cosine").diagonal()
+    cosim_matrix = 1 - distance.cdist(
+        query_mat, embedding_mat, "cosine").diagonal()
 
     translations = []
 
-    texts = list(
-        df.reset_index().iloc[cosim_matrix.argsort()[-5:][::-1]].answer.values
-    )
-    bs = np.round(cosim_matrix[cosim_matrix.argsort()[-5:][::-1]], 2)
+    picked_items = cosim_matrix.argsort()[-5:][::-1]
+    answers = list(df.reset_index().iloc[picked_items].answer.values)
+    questions = list(df.reset_index().iloc[picked_items].question.values)
+    bs = np.round(cosim_matrix[picked_items], 2)
 
-    for a, b in zip(texts, bs):
+    for answer, question, b in zip(answers, questions, bs):
+        translated_answer, updated, source_link = translate_answer(answer, lang)
 
-        translation = translator.translate(a, dest=lang)
-        text = translation.text
+        translated_question = translate(question, lang)
 
-        blob_match = BLOB_RE.match(text)
-        if blob_match is not None:
+        result = {
+            "answer": ". ".join(
+                [scentence.capitalize() for scentence in
+                 translated_answer.split(". ")]),
+            "question": translated_question,
+            "updated": updated,
+            "source_link": source_link,
+            "b": b,
+        }
 
-            url = blob_match["url"]
-            if url is not None:
-                url = url.replace(" ", "").replace(",", "")
-
-            result = {
-                "text": blob_match["text"].capitalize(),
-                "updated": blob_match["updated"],
-                "url": url,
-                "b": b,
-            }
-
-            translations.append(result)
-        # else:
-        #     print(text)
+        translations.append(result)
 
     return render_template(
         "hello.html",
